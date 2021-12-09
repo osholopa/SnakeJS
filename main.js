d33e4e2fb0b796f18da721ae357948fd0480713b
Oskari Holopainen <oskariholopa@gmail.com>
2021-12-09 19:24:17 +0200

    Add audio, touch controls and update UI

--- main.js
+++ main.js
@@ -1,18 +1,106 @@
-const canvas = document.getElementById("game")
-const ctx = canvas.getContext("2d")
-let frameTime = 90
+//////////////// Audio & Audio Context ///////////////
+const AudioContext = window.AudioContext || window.webkitAudioContext
+const audioCtx = new AudioContext()
+const audioElement = document.querySelector('audio')
+const track = audioCtx.createMediaElementSource(audioElement)
+const gainNode = audioCtx.createGain();
+gainNode.gain.value = 0.5
+track.connect(audioCtx.destination)
+
+
+//////////////// Canvas & Render Context ///////////////
+const canvas = document.getElementById('game')
+const ctx = canvas.getContext('2d')
+
+//////////////// Variables ///////////////
+let touchStartY
+let touchStartX
+let lastTouchDirection
+const frameTime = 90
 const gridWidthInCells = 50
 const cellSize = canvas.width / gridWidthInCells
-////////////////Oliot, Eventlistenerit, Const-määrittelyt///////////////
-let game = {
-  score: 0
+
+// Game Objects
+const game = {
+  score: 0,
+}
+
+const snake = {
+  parts: [{ x: 10, y: 10 }],
+  direction: 'RIGHT',
+  grow: function () {
+    this.parts.push({
+      x: snake.parts[snake.parts.length - 1].x,
+      y: snake.parts[snake.parts.length - 1].y,
+    })
+  },
 }
 
 const apple = {
   x: 19,
   y: 10,
-  alive: true
+  alive: true,
 }
+
+const keysDown = {}
+
+//////////////// Eventlisteners ///////////////
+window.addEventListener('load', startGame)
+
+window.addEventListener('keydown', (e) => {
+  if (audioCtx.state === 'suspended') {
+		audioCtx.resume();
+	}
+
+  const pressedKey = e.key
+  for (let key in keysDown) {
+    keysDown[key] = false
+  }
+  keysDown[pressedKey] = true
+})
+
+window.addEventListener('keyup', (e) => {
+  const key = e.key
+  keysDown[key] = false
+})
+
+
+
+window.addEventListener('touchstart', (e) => {
+  if (audioCtx.state === 'suspended') {
+		audioCtx.resume();
+	}
+  
+  touchStartY = e.touches[0].clientY
+  touchStartX = e.touches[0].clientX
+})
+
+window.addEventListener('touchmove', (e) => {
+  const touchEndY = e.touches[0].clientY
+  const touchEndX = e.touches[0].clientX
+
+  if (['RIGHT', 'LEFT'].includes(snake.direction)) {
+    if (touchStartY < touchEndY) {
+      lastTouchDirection = 'DOWN'
+    } else {
+      lastTouchDirection = 'UP'
+    }
+  } else {
+    if (touchStartX < touchEndX) {
+      lastTouchDirection = 'RIGHT'
+    } else {
+      lastTouchDirection = 'LEFT'
+    }
+  }
+})
+
+window.addEventListener('touchend', () => {
+  if(lastTouchDirection !== snake.direction) snake.direction = lastTouchDirection
+})
+
+
+
+////////////// Functions ///////////////////
 
 function newApple() {
   apple.x = Math.floor(Math.random() * gridWidthInCells)
@@ -20,55 +108,9 @@ function newApple() {
   apple.alive = true
 }
 
-const keysDown = {}
-
-window.addEventListener("load", startGame)
-
-window.addEventListener("keydown", e => {
-  const pressedKey = e.key
-  for(var key in keysDown) {
-    keysDown[key] = false
-  }
-  keysDown[pressedKey] = true
-})
-
-window.addEventListener("keyup", e => {
-  const key = e.key
-  keysDown[key] = false
-})
-
-const snake = {
-  parts: [{ x: 10, y: 10 }],
-  direction: "RIGHT",
-  grow: function() {
-    this.parts.push({
-      x: snake.parts[snake.parts.length - 1].x,
-      y: snake.parts[snake.parts.length - 1].y
-    })
-  }
-}
-
-const handleButtonPress = dir => {
-  switch (dir) {
-    case "RIGHT":
-      snake.direction = "RIGHT"
-      break
-    case "DOWN":
-      snake.direction = "DOWN"
-      break
-    case "LEFT":
-      snake.direction = "LEFT"
-      break
-    case "UP":
-      snake.direction = "UP"
-      break
-  }
-}
-
-//////////////Funktiot///////////////////
 function drawSnake(snake) {
-  ctx.fillStyle = "black"
-  snake.parts.forEach(part => {
+  ctx.fillStyle = 'black'
+  snake.parts.forEach((part) => {
     const xInPixels = part.x * cellSize
     const yInPixels = part.y * cellSize
     ctx.fillRect(xInPixels, yInPixels, cellSize, cellSize)
@@ -78,26 +120,26 @@ function drawApple(apple) {
   if (apple.alive === true) {
     const xInPixels = apple.x * cellSize
     const yInPixels = apple.y * cellSize
-    ctx.fillStyle = "red"
+    ctx.fillStyle = 'red'
     ctx.fillRect(xInPixels, yInPixels, cellSize, cellSize)
   }
 }
 
 function inputHandler() {
-  if (snake.direction === "UP" || snake.direction === "DOWN") {
+  if (snake.direction === 'UP' || snake.direction === 'DOWN') {
     if (keysDown.ArrowLeft) {
-      snake.direction = "LEFT"
+      snake.direction = 'LEFT'
     }
     if (keysDown.ArrowRight) {
-      snake.direction = "RIGHT"
+      snake.direction = 'RIGHT'
     }
   }
-  if (snake.direction === "LEFT" || snake.direction === "RIGHT") {
+  if (snake.direction === 'LEFT' || snake.direction === 'RIGHT') {
     if (keysDown.ArrowUp) {
-      snake.direction = "UP"
+      snake.direction = 'UP'
     }
     if (keysDown.ArrowDown) {
-      snake.direction = "DOWN"
+      snake.direction = 'DOWN'
     }
   }
 }
@@ -123,23 +165,20 @@ function moveSnake() {
   const tail = snake.parts[snake.parts.length - 1]
   const head = snake.parts[0]
   switch (snake.direction) {
-    case "RIGHT":
-      //määritellään hännän x olevan yhtä kuin pään x + 1, jolloin taulukon vika alkio napataan käärmeen pääksi
+    case 'RIGHT':
+      
       tail.x = head.x + 1
       tail.y = head.y
       break
-    case "DOWN":
-      //määritellään hännän y olevan yhtä kuin..
+    case 'DOWN':
       tail.y = head.y + 1
       tail.x = head.x
       break
-    case "LEFT":
-      //määritellään hännän x olevan yhtä kuin..
+    case 'LEFT':
       tail.x = head.x - 1
       tail.y = head.y
       break
-    case "UP":
-      //määritellään hännän y olevan yhtä kuin..
+    case 'UP':
       tail.y = head.y - 1
       tail.x = head.x
       break
@@ -151,13 +190,14 @@ function eatApple() {
   if (snake.parts[0].x === apple.x && snake.parts[0].y === apple.y) {
     apple.alive = false
     snake.grow()
+    if(audioCtx.state === 'running') audioElement.play()
     newApple()
     game.score += 100
   }
 }
 
 function renderScore() {
-  document.getElementById("score").innerHTML = "Score: " + game.score
+  document.getElementById('score').innerHTML = 'Score: ' + game.score
 }
 
 function loseGame() {
@@ -166,18 +206,17 @@ function loseGame() {
     for (i = 1; i < snake.parts.length; i++) {
       let part = snake.parts[i]
       if (head.x === part.x && head.y === part.y) {
-        if (
-          window.confirm(
-            `Game Over! Your score is  + ${game.score}. Play again?`
-          )
-        ) {
-          location.reload()
-        } else {
-          window.close()
-        }
+        alert(`Game Over! Your score is  + ${game.score}.`)
+        reset()
       }
     }
   }
+}
+
+function reset() {
+  snake.parts = [{ x: 10, y: 10 }]
+  snake.direction = 'RIGHT'
+  location.reload()
 }
 
 function updateGame() {
